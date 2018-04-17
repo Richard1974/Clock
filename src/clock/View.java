3f880b51d9ffb4275fde666aaf331f50a0bb09ca
RichardColdwell <15017492@uhi.ac.uk>
2018-04-17 15:36:00 +0100

    Made changes to layout in View Class

--- src/clock/View.java
+++ src/clock/View.java
@@ -12,36 +12,27 @@ public class View implements Observer {
     public View(Model model) {
         JFrame frame = new JFrame();
         panel = new ClockPanel(model);
-        //frame.setContentPane(panel);
         frame.setTitle("Java Clock");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         // Start of border layout code
-        
-        // I've just put a single button in each of the border positions:
-        // PAGE_START (i.e. top), PAGE_END (bottom), LINE_START (left) and
-        // LINE_END (right). You can omit any of these, or replace the button
-        // with something else like a label or a menu bar. Or maybe you can
-        // figure out how to pack more than one thing into one of those
-        // positions. This is the very simplest border layout possible, just
-        // to help you get started.
-        
         Container pane = frame.getContentPane();
         
-        JButton button = new JButton("Button 1 (PAGE_START)");
-        pane.add(button, BorderLayout.PAGE_START);
-         
         panel.setPreferredSize(new Dimension(200, 200));
-        pane.add(panel, BorderLayout.CENTER);
-         
-        button = new JButton("Button 3 (LINE_START)");
+        pane.add(panel, BorderLayout.PAGE_START);
+        
+        JButton button = new JButton("LOAD");
+        pane.add(button, BorderLayout.CENTER);
+                 
+        button = new JButton("ALARMS");
         pane.add(button, BorderLayout.LINE_START);
+        
          
-        button = new JButton("Long-Named Button 4 (PAGE_END)");
-        pane.add(button, BorderLayout.PAGE_END);
-         
-        button = new JButton("5 (LINE_END)");
+        button = new JButton("SAVE");
         pane.add(button, BorderLayout.LINE_END);
+        
+        button = new JButton("TBA");
+        pane.add(button, BorderLayout.PAGE_END);
         
         // End of borderlayout code
         
