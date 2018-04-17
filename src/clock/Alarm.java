b6255786b76007d19037534b350beb98ac3e21e8
RichardColdwell <15017492@uhi.ac.uk>
2018-04-17 15:05:10 +0100

    created first draft of alarm class

--- src/clock/Alarm.java
+++ src/clock/Alarm.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package clock;
 
-/**
- *  Alarm Class
- *  used 
- */
-public class Alarm {
+import java.util.Calendar;
+//********************************************************
+//**  Alarm Class                                       **
+//**  alarm object used to store details of the alarm   **
+//**  @author Richard Coldwell                          **
+//**  17th April 2018                                   **
+//********************************************************
+
+
+public class Alarm 
+{
+    Calendar alarm;
     
+    public Alarm(Calendar alarm)
+    {
+        this.alarm = alarm;
+    }
+
+    public Calendar getAlarm() {
+        return alarm;
+    }
+
+    @Override
+    public String toString() {
+        return getAlarm().toString();
+    }
 }
