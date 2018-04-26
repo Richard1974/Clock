435f1852df6c982240e058a5b03e1faf7ca84543
RichardColdwell <15017492@uhi.ac.uk>
2018-04-26 16:14:13 +0100

    Added Spinners for Hours and minutes

--- src/clock/AddAlarmDialog.java
+++ src/clock/AddAlarmDialog.java
@@ -1,11 +1,14 @@
 package clock;
 
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerDateModel;
 import javax.swing.SpinnerNumberModel;
@@ -19,9 +22,9 @@ public class AddAlarmDialog extends JDialog{
     
     private JButton addButton;
     private JButton cancelButton;
-    private JSpinner hour;
+    private JSpinner hourSpinner;
     private SpinnerNumberModel hourModel;
-    private JSpinner minute;
+    private JSpinner minuteSpinner;
     private SpinnerNumberModel minuteModel;
     
     
@@ -32,7 +35,29 @@ public class AddAlarmDialog extends JDialog{
         
         addButton = new JButton("Add");
         cancelButton = new JButton("Cancel");
+        hourModel = new SpinnerNumberModel(1, 1, 12, 1);
+        hourSpinner = new JSpinner(hourModel);
+        minuteModel = new SpinnerNumberModel(0, 0, 59, 1);
+        minuteSpinner = new JSpinner(minuteModel);
         
+        setLayout(new GridBagLayout());
+        
+        GridBagConstraints gc = new GridBagConstraints();
+        gc.gridy = 0;
+        gc.weightx = 1;
+        gc.weighty = 1;
+        gc.fill = GridBagConstraints.NONE;
+        
+        gc.gridx = 0;
+        add(new JLabel ("Hours"), gc);
+        gc.gridx ++;
+        add(new JLabel ("Minutes"), gc);
+       
+        gc.gridy ++;
+        gc.gridx = 0;
+        add(hourSpinner, gc );
+        gc.gridx ++;
+        add(minuteSpinner, gc );
         
         setSize(200,200);
         
