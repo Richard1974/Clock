d2a9dd68c437ca8f8a68cd793fa24d6dfd82e81c
RichardColdwell <15017492@uhi.ac.uk>
2018-05-10 14:26:57 +0100

    Sorted Array List Functionality All working

--- src/clock/SortedArrayPriorityQueue.java
+++ src/clock/SortedArrayPriorityQueue.java
@@ -7,9 +7,6 @@ package clock;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -48,134 +45,63 @@ public class SortedArrayPriorityQueue<T>implements PriorityQueue<Date> {
         tailIndex = -1;
     }
     
-    
     @Override
     public Date head() throws QueueUnderflowException 
     {
-        if (isEmpty()) {
+        if (isEmpty()) 
+        {
             throw new QueueUnderflowException();
-        } else {
-            
+        } else 
+        {
             return ((PriorityItem<T>) storage[0]).getAlarm();
-            
         }
     }
 
     @Override
     public void add(Date alarm) throws QueueOverflowException {
         tailIndex = tailIndex + 1;
-        if (tailIndex >= capacity) {
+        if (tailIndex >= capacity) 
+        {
             /* No resizing implemented, but that would be a good enhancement. */
             tailIndex = tailIndex - 1;
             throw new QueueOverflowException();
         } else {
             /* Scan backwards looking for insertion point */
             int i = tailIndex;
-            
-            
-           
             while (i > 0 && checkDate(i,  DateConvert (storage[i - 1]), alarm )) 
             {
-               
                 storage[i] = storage[i - 1];
                 i = i - 1;
-                
             }
             storage[i] = new PriorityItem<>(alarm);
             System.out.println("storage[0] = " + storage[0]);
-           
-            }
-          
+        } 
     }
-    
-   
-        public Date DateConvert(Object date)
-        {
-            
-           
-            
-            
-            
-            Date dateFormatted = null;
-            String stringDate = date.toString();
-            System.out.println("stringDate= " + stringDate);
-            
-            //             EEE MMM dd HH:mm:ss z yyyy
-            //stringDate = "Thu May 10 11:30:28 BST 2018";
-            SimpleDateFormat formatter = new SimpleDateFormat("(EEE MMM dd HH:mm:ss zzz yyyy)");
-        
-
-        try {
-
-            dateFormatted = formatter.parse(stringDate);
-            System.out.println(dateFormatted);
-            
-            } 
-        catch (ParseException e)
-            {
-                e.printStackTrace();
-            }
-            
-            
-          
-            return dateFormatted;
-        }
-    
-        public boolean checkDate (int i, Date oldDate, Date newDate)
-        {
-
-            if (oldDate.after(newDate))
-            {
-                return true;
-            }
-
-            else
-            {
-                return false;
-            }
-
-
-
-        }
-                
-    
-    
     
     @Override
     public void remove() throws QueueUnderflowException {
-        
-      /*  if (isEmpty()) {
+        if (isEmpty()) 
+        {
             throw new QueueUnderflowException();
-        } else {
-            // Finds the item in the array with the highest priority value
-            int highestPriority = ((PriorityItem<T>) storage[0]).getPriority();
-            int highestPriorityIndex = 0;
-            for (int i = 0; i <= tailIndex; i++) {
-                if (((PriorityItem<T>) storage[i]).getPriority() > highestPriority)
-                {
-                    highestPriority = ((PriorityItem<T>) storage[i]).getPriority();
-                    highestPriorityIndex = i;
-                }
-            }
-            // starting at the point that the item was removed from it now shifts all items 
-            // after this by 1 space to fill in the gap
-            int i = highestPriorityIndex;
-            while (i<tailIndex){
-            
+        } else 
+        {
+            for (int i = 0; i < tailIndex; i++) 
+            {
                 storage[i] = storage[i + 1];
-                i = i + 1;
             }
             tailIndex = tailIndex - 1;
-        }*/
+        }
     }
     
     @Override
-    public boolean isEmpty() {
+    public boolean isEmpty() 
+    {
         return tailIndex < 0;
     }
 
     @Override
-    public String toString() {
+    public String toString() 
+    {
         String result = "[";
         for (int i = 0; i <= tailIndex; i++) {
             if (i > 0) {
@@ -185,5 +111,38 @@ public class SortedArrayPriorityQueue<T>implements PriorityQueue<Date> {
         }
         result = result + "]";
         return result;
+    }
+    
+    //converts the Object Date into a Date that can beu sed for comparison
+    public Date DateConvert(Object date)
+    {
+        Date dateFormatted = null;
+        String stringDate = date.toString();
+        System.out.println("stringDate= " + stringDate);
+
+        SimpleDateFormat formatter = new SimpleDateFormat("(EEE MMM dd HH:mm:ss zzz yyyy)");
+        try 
+        {
+            dateFormatted = formatter.parse(stringDate);
+            System.out.println(dateFormatted);
+        }
+        catch (ParseException e)
+        {
+            e.printStackTrace();
+        }
+        return dateFormatted;
+    }
+    
+    //check if the stored Date(oldDate) is after the newDate 
+    public boolean checkDate (int i, Date oldDate, Date newDate)
+    {
+        if (oldDate.after(newDate))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
